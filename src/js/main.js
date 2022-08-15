1e826b371312fb48ce9f7d45d721a38544b2f931
CristinaH.Sansaloni <aacchhss@hotmail.com>
2022-08-15 13:14:31 +0200

    fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
-console.log('>> Ready :)');
+//De html
+const listUl = document.querySelector('.js_list');
+
+//Globales
+let data = [];
+
+
+
+//1. Código cuando carga la página
+fetch('https://randomuser.me/api/?results=10')
+  .then((response) => response.json()) 
+  .then((dataAPI) => {
+    console.log(dataAPI);
+
+    data = dataAPI.results.map((eachUser) => ({
+      //id: eachUser.id.value,
+      fullName: eachUser.name.first + ' ' + eachUser.name.last,
+      city: eachUser.location.city,
+      photo: eachUser.picture.medium,
+      username: eachUser.login.username,
+      isFriend: false,
+    }));
+
+    console.log(data);
+
+    renderUsers();
+    //addCardListeners(); //vuelvo a escuchar
+    
+  });
+
+
+
+
+  function renderUsers() {
+
+  }
